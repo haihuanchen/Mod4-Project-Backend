dffaced1111c60b1f9b911088ec331fb3bab3227
brindab50 <brindab50@gmail.com>
2020-05-01 15:14:13 -0400

    secondcommit

--- BackEnd/SourceCode/db/seeds.rb
+++ BackEnd/SourceCode/db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+
+10.times do 
+    User.create(
+        name: Faker::Name.name,
+        username: Faker::Internet.username
+        password: 123
+        email: Faker::Internet.email
+
+
+    )
+l1 = Language.create(title: "JavaScript", description: "JavaScript, often abbreviated as JS, is a programming language that conforms to the ECMAScript specification. JavaScript is high-level, often just-in-time compiled, and multi-paradigm. It has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.", resources: "https://developer.mozilla.org/en-US/docs/Web/JavaScript")
+l2 = Language.create(title: "Ruby", description: "Ruby is an interpreted, high-level, general-purpose programming language. It was designed and developed in the mid-1990s by Yukihiro "Matz" Matsumoto in Japan. Ruby is dynamically typed and uses garbage collection. It supports multiple programming paradigms, including procedural, object-oriented, and functional programming. According to the creator, Ruby was influenced by Perl, Smalltalk, Eiffel, Ada, Basic, and Lisp.", resources: "https://www.ruby-lang.org/en/documentation/")
+
+
+    10.times do 
+        Post.create(
+            title: 
+            content: 
+            user_id:
+            language_id:
+
+        )
